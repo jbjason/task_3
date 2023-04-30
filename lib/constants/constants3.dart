b225f31a329671e7a71a49b1ad81434ba9d085d5
Jubayer <62088928+jbjason@users.noreply.github.com>
2023-05-01 00:02:09 +0600

    Initial commit

---
+++ lib/constants/constants3.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const backColor = Color(0xFFF8F8F8);
+const secondaryColor = Color(0xFFF05A76);
+const cardPrimaryColor = Color(0xFFFFFFFF);
+const cardSecondaryColor = Color(0xFFEEEEEE);
+
+const textPrimaryColor = Colors.black;
+const textSecondaryColor = Colors.black38;
+
+const groups = [
+  'assets/icons/group1.png',
+  'assets/icons/group2.png',
+  'assets/icons/group3.png',
+  'assets/icons/group4.png',
+];
+
+const category = 'assets/icons/category.png';
---
+++ lib/screens/home_screen3.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen3 extends StatelessWidget {
+  const HomeScreen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
